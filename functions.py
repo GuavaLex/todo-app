98c412c116b488a861641dcd6ed0e7b66b19c690
guava <guavareigen2@gmail.com>
2023-01-30 15:36:59 +0530

    Implemented add function

--- functions.py
+++ functions.py
@@ -1,10 +1,12 @@
-def get_todos(filepath="todos.txt"):
+
+FILEPATH = "todos.txt"
+def get_todos(filepath=FILEPATH):
     """"Read the text file and return the list"""
     with open(filepath, "r") as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(todos_arg, filepath="todos.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
     with open(filepath, "w") as file:
         file.writelines(todos_arg)
 
--- gui.py
+++ gui.py
@@ -2,9 +2,23 @@ import functions
 import PySimpleGUI as sg
 
 Label = sg.Text("Enter the todo")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key = "todo")
 btn = sg.Button("Add")
 
-window = sg.Window("My To-do App", layout=[[Label], [input_box ,btn ]])
-window.read()
+window = sg.Window("My To-do App",
+                   layout=[[Label], [input_box ,btn ]],
+                   font=("Helvetica", 20))
+while True:
+    event, value = window.read()
+    print(event)
+    print(value)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todos = value["todo"] + "\n"
+            todos.append(new_todos)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
